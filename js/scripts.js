93ccc442d89833dbfa23fb378c6d6ab66bb49f25
Deya Card <thedeyacard@gmail.com>
2022-02-07 19:49:03 -0800

    create tests 1-3

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function noInputtedNumber() {
+  for (let i=0; i < arguments.length; i++) {
+    if (arguments[i].trim().length === 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function creditCardValidator(number) {
+  if (noInputtedNumber(number)) {
+    return "This card number in not valid";
+  }
+  else{ if ( number.length > 16 || number.length < 14) {
+    return "This card number in not valid";
+  }
+
+  }
+}
+
+
+
