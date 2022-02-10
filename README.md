46113ef84d794c944d4542c4f897acc4474db6f7
Deya Card <thedeyacard@gmail.com>
2022-02-09 18:15:34 -0800

    work in progress

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Describe: creditCardValidator()
 
 Test: "It ignores non numbers since they cannot be in credit cards."
-Code: creditCardValidator()
+Code: creditCardValidator(A)
 Expected Output: "This card number in not valid" 
 
 Test: "If you don't enter anything, the card will not be vaild."
@@ -9,12 +9,28 @@ Code: creditCardValidator()
 Expected Output: "This card number is not valid"
 
 Test: "If an entry is longer that 16 numbers, the card will not be valid."
-Code: creditCardValidator()
+Code: creditCardValidator(41020808604356201)
 Expected Output: "This card number is not valid"
 
 Test: "If an entry is shorter than 14 numbers, the card will not be valid."
-Code: creditCardValidator()
+Code: creditCardValidator(4102080860435)
 Expected Output: "This card number is not valid"
+
+Test: "It will move right to left, doubling every other number.
+Code: creditCardValidator(1,2,3,4,5)
+Expected Output: 2,2,6,4,10
+
+Test: "If the result of the doubled digit is a double digit number, add together each digit of the double digit number."
+Code: creditCardValidator(7)
+Expected Output: 5
+
+Test: "Sum all of the digits in the new transformed set of numbers."
+Code: creditCardValidator(1,2,3,4,5)
+Expected Output: 24
+
+Test: "If the resulting number ends in a zero, the card is valid."
+Code: creditCardValidator(4102080860435620)
+Expected Output: "This card number is valid."
 
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,54 @@
-function noInputtedNumber() {
-  for (let i=0; i < arguments.length; i++) {
-    if (arguments[i].trim().length === 0) {
-      return true;
+const onlyNumbers = ["0","1","2","3","4","5","6","7","8","9"]
+
+function creditCardValidator(purportedCC) {
+  if (purportedCC.length === 0) {
+    return "This card number is not valid";
+  }else if( purportedCC.length > 16 || purportedCC.length < 14) {
+    return "This card number is not valid";
+  }
+
+  for(let i = 0; i<purportedCC.length;i++) {
+    if (onlyNumbers.indexOf(purportedCC[i]) === -1){
+      return "This card number is not valid";
     }
   }
-  return false;
-}
-
-function creditCardValidator(number) {
-  if (noInputtedNumber(number)) {
-    return "This card number in not valid";
-  }
-  else{ if ( number.length > 16 || number.length < 14) {
-    return "This card number in not valid";
-  }
-
-  return "This card number is valid."
+  for (let i = purportedCC.length; i >= 0; i--){
+    console.log(purportedCC[i])
   }
 }
 
 
+      //Begin algorhythm
 
+
+
+  // let nDigits = purportedCC.length
+  // let sum = parseInt(purportedCC[nDigits-1])
+  // let parity = (nDigits-2)  %  2
+  // for (let i = 0; i < nDigits -2; i++) {
+  //   let digit = parseInt(purportedCC[i])
+  //   if (i % 2 === parity){
+  //     digit = digit * 2
+  //   } 
+  //   if (digit > 9) {
+  //     digit = digit - 9
+  //   }
+  //   sum = sum + digit
+  // }
+  // return (sum % 10)
+
+
+// function checkLuhn(string purportedCC) {
+//   int nDigits := length(purportedCC)
+//   int sum := integer(purportedCC[nDigits-1])
+//   int parity := (nDigits-2) modulus 2
+//   for i from 0 to nDigits - 2 {
+//       int digit := integer(purportedCC[i])
+//       if i modulus 2 = parity
+//           digit := digit × 2
+//       if digit > 9
+//           digit := digit - 9 
+//       sum := sum + digit
+//   }
+//   return (sum modulus 10) = 0
+// }
